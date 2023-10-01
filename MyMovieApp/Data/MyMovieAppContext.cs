37f096daefde5b61fea4298d6c8177d13b2bb111
Sunday Oladiran <Sunday.oladiran@wemabank.com>
2023-10-02 00:25:28 +0100

    initial commit

---
+++ MyMovieApp/Data/MyMovieAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyMovieApp.Models;
+
+namespace MyMovieApp.Data
+{
+    public class MyMovieAppContext : DbContext
+    {
+        public MyMovieAppContext (DbContextOptions<MyMovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyMovieApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
